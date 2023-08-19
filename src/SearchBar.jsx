e06ab01cea41bc21f0bc017c63b25f6422722e6a
modusami <128193218+modusami@users.noreply.github.com>
2023-08-19 11:08:43 -0400

    Replace COL size

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,17 +2,18 @@ function SearchBar({ location, setLocation }) {
 	return (
 		<>
 			<div id="searchBar" className="container">
-				<div className="row">
-					<div className="col text-center">
-						<span id="searchIconSpan" className="text-center">
+				<div className="row g-0">
+					<div className="col-1 text-center">
+						<span id="searchIconSpan" className="text-center d-block">
 							<i className="fa-solid fa-search"></i>
 						</span>
 					</div>
-					<div className="col">
+					<div className="col-10">
 						<input
 							type="text"
 							placeholder="Enter a country..."
 							value={location}
+							className="form-control w-100"
 							onChange={(e) => setLocation(e.target.value)}
 						/>
 					</div>
