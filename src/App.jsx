0db53717dc8517b439b3374814ba0305e4b0c97a
modusami <128193218+modusami@users.noreply.github.com>
2023-08-19 12:15:42 -0400

    Add More Use States And A Use Effect For The DropDown...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import WeatherApp from "./WeatherApp";
 
@@ -11,20 +11,39 @@ import sunny from "./assets/sunny.svg";
 
 function App() {
 	const [location, setLocation] = useState("");
+	const [activeLocation, setActiveLocation] = useState("");
 	const [scaleF, setScale] = useState(true); // true meaning fahrenheit
 	const [temp, setTemp] = useState("0.0");
 	const weeatherImages = [cloudy, lighting, rain, snowing, sunny];
 	const [activeWeatherImg, setActiveWeatherImg] = useState(0);
+	const [searchDropdown, setShowSearchDropdown] = useState(false);
+
+	function handleOnChangeSearch(e) {
+		setLocation(e.target.value);
+	}
+
+	const handleOnClickSearchButton = (e) => {
+		setActiveLocation(location);
+	};
+
+	useEffect(() => {
+		location === "" ? setShowSearchDropdown(false) : setShowSearchDropdown(true);
+	}, [location]);
 
 	return (
 		<>
 			<div id="weatherDiv">
 				<div id="searchBarWrapper">
-					<SearchBar location={location} setLocation={setLocation} />
+					<SearchBar
+						location={location}
+						handleChange={handleOnChangeSearch}
+						onSearchButton={handleOnClickSearchButton}
+						showDropDown={searchDropdown}
+					/>
 				</div>
 				<div id="weatherWrapper">
 					<WeatherApp
-						location={location}
+						location={activeLocation}
 						scaleF={scaleF}
 						temp={temp}
 						img={weeatherImages[activeWeatherImg]}
