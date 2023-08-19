b41052cf4882479a264ce4307c74ae7ba0b0509b
modusami <128193218+modusami@users.noreply.github.com>
2023-08-19 11:33:52 -0400

    add usestates and etc for mockup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 import SearchBar from "./SearchBar";
 import WeatherApp from "./WeatherApp";
 
+// all images to use
+import cloudy from "./assets/cloudy.svg";
+import lighting from "./assets/lightning.svg";
+import rain from "./assets/rain.svg";
+import snowing from "./assets/snowing.svg";
+import sunny from "./assets/sunny.svg";
+
 function App() {
 	const [location, setLocation] = useState("");
 	const [scaleF, setScale] = useState(true); // true meaning fahrenheit
 	const [temp, setTemp] = useState("0.0");
+	const weeatherImages = [cloudy, lighting, rain, snowing, sunny];
+	const [activeWeatherImg, setActiveWeatherImg] = useState(0);
 
 	return (
 		<>
@@ -14,7 +23,12 @@ function App() {
 					<SearchBar location={location} setLocation={setLocation} />
 				</div>
 				<div id="weatherWrapper">
-					<WeatherApp location={location} scaleF={scaleF} temp={temp} />
+					<WeatherApp
+						location={location}
+						scaleF={scaleF}
+						temp={temp}
+						img={weeatherImages[activeWeatherImg]}
+					/>
 				</div>
 			</div>
 		</>
