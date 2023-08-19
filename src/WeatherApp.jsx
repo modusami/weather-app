8fea1e179f14110bc85d2a3988b0074fa5c851ac
modusami <128193218+modusami@users.noreply.github.com>
2023-08-19 14:59:49 -0400

    add a hyphen between the temp and scale

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,10 +1,11 @@
 function WeatherApp({ img, temp, scaleF, location }) {
 	return (
 		<>
-			<div id="weatherApp" className="container border">
-				<div className="row">
-					<div className="col-12 text-center fs-3">
-						<span className="temp">{temp ? temp : "0"}</span>{" "}
+			<div id="weatherApp" className="container">
+				<div id="tempRow" className="row">
+					<div className="col-12 text-center fw-bold d-flex justify-content-center">
+						<span className="temp">{temp ? temp : "0"}</span>
+						{" - "}
 						<span className="scale">{scaleF ? "F" : "C"}</span>
 					</div>
 				</div>
@@ -23,7 +24,7 @@ function WeatherApp({ img, temp, scaleF, location }) {
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-12 d-flex align-items-end justify-content-center">
+					<div className="col-12 d-flex align-items-end justify-content-center text-center">
 						<h2>{location ? location : "Waiting..."}</h2>
 					</div>
 				</div>
