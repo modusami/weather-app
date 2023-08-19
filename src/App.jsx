47a05149f01bf752f9531d40adecdc376ffeaa0d
modusami <128193218+modusami@users.noreply.github.com>
2023-08-19 11:17:06 -0400

    Add Weather App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import SearchBar from "./SearchBar";
+import WeatherApp from "./WeatherApp";
 
 function App() {
 	const [location, setLocation] = useState("");
 
 	return (
 		<>
-			<div id="weatherApp">
+			<div id="weatherDiv">
 				<div id="searchBarWrapper">
 					<SearchBar location={location} setLocation={setLocation} />
 				</div>
-				<div id="weatherWrapper"></div>
+				<div id="weatherWrapper">
+					<WeatherApp />
+				</div>
 			</div>
 		</>
 	);
